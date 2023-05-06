63a18fe798e9cfbd4555a44206a452770bf33893
Alexandp L <alexandp83@mail.ru>
2023-05-06 11:05:54 +0300

    Добавил number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -19,3 +19,13 @@ class Phone(Item):
     def __add__(self, other):
         if isinstance(self, Phone):
             return self.quantity + other.quantity
+
+    @property
+    def number_of_sim(self) -> int:
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int) -> None:
+        if value == 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self._number_of_sim = value
